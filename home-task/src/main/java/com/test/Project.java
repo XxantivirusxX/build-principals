9b183af4010485f79d1da3ced97a4aad35aa020f
Tsyuliou <tsyuliou@gmail.com>
2019-03-17 02:35:41 +0300

    check my job

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -6,7 +6,7 @@ import org.apache.log4j.Logger;
 class Project {
 
  public String getGreeting() {
-        return "Hello, MNT Lab!";
+        return "Hello, MNT Lab! by atsyuliou";
     }
 
   public static void test() {
